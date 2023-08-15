523a0985d880670f7e601235d4f9c4de365a5007
Ian Felix <ianfelix030@gmail.com>
2023-08-15 00:58:04 -0300

    refactor: add steps dynamically using array from and step value

--- src/components/StepSphere/index.tsx
+++ src/components/StepSphere/index.tsx
@@ -1,10 +1,19 @@
 import { View } from 'react-native';
 
-export const StepSphere = ({ step }: { step: number }) => (
-  <View className="flex-row items-center justify-center gap-1">
-    <View className={`h-2 w-2 rounded-full ${step === 1 ? 'bg-brand' : 'bg-gray-200'}`} />
-    <View className={`h-2 w-2 rounded-full ${step === 2 ? 'bg-brand' : 'bg-gray-200'}`} />
-    <View className={`h-2 w-2 rounded-full ${step === 3 ? 'bg-brand' : 'bg-gray-200'}`} />
-    <View className={`h-2 w-2 rounded-full ${step === 4 ? 'bg-brand' : 'bg-gray-200'}`} />
-  </View>
-);
+export const StepSphere = ({ step }: { step: number }) => {
+  return (
+    <View className="flex-row items-center justify-center">
+      {Array.from({ length: 5 }, (_, i) => i++).map((value) => {
+        return (
+          <View className="mx-[2px]" key={value}>
+            {step === value + 1 ? (
+              <View className="h-2 w-2 rounded-full bg-brand" />
+            ) : (
+              <View className="h-2 w-2 rounded-full bg-gray-200" />
+            )}
+          </View>
+        );
+      })}
+    </View>
+  );
+};
