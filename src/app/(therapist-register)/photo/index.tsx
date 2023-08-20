7eb2b11957ca2861b6c453aa9d7fb6459d00d0a2
Ian Felix <ianfelix030@gmail.com>
2023-08-20 02:36:54 -0300

    feat: create photo and take photo screen

---
+++ src/app/(therapist-register)/photo/index.tsx
@@ -0,0 +1,56 @@
+import { Feather } from '@expo/vector-icons';
+import { Image } from 'expo-image';
+import { Link, router } from 'expo-router';
+import { Text, View } from 'react-native';
+
+import { Button } from '@/components/Button';
+import { CardStack } from '@/layout/CardStack';
+import colors from '@/theme/colors';
+
+export default function Photo() {
+  return (
+    <View className="w-full flex-1 items-center justify-center">
+      <CardStack>
+        <View className="flex-1 items-center justify-center rounded-3xl bg-white px-4 pb-8 pt-10">
+          <View className="flex-1 items-center justify-center">
+            <Image
+              className="h-52 w-52"
+              source={require('@/assets/images/walkthrough-step-2.svg')}
+            />
+
+            <View className="mt-6 space-y-7">
+              <Text className="text-center font-MontserratBold text-xl">
+                Hora de adicionar foto de perfil profissional
+              </Text>
+              <Text className=" text-center font-MontserratMedium text-base">Capriche na foto</Text>
+            </View>
+          </View>
+
+          <View className="w-full flex-row justify-between">
+            <Button className="bg-gray-100" variant="rounded" onPress={() => router.back()}>
+              <Feather
+                name="arrow-left"
+                size={24}
+                color={colors.gray[600]}
+                backgroundColor="transparent"
+              />
+            </Button>
+            <Link asChild href="/(therapist-register)/photo/takePhoto/">
+              <Button className="w-full max-w-[208px]">
+                <View className="flex-row items-center gap-4">
+                  <Text className="font-MontserratBold text-base text-white">Adicionar foto</Text>
+                  <Feather
+                    name="arrow-right"
+                    size={24}
+                    color="white"
+                    backgroundColor="transparent"
+                  />
+                </View>
+              </Button>
+            </Link>
+          </View>
+        </View>
+      </CardStack>
+    </View>
+  );
+}
