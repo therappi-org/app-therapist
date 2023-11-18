7fc973da815fc63e72176369ec2b07bc361c3acb
Ian Felix <ianfelix030@gmail.com>
2023-11-17 21:16:47 -0300

    refactor: add KeyBoardAvoidingViewLayout as layout

--- src/app/(auth)/create-account/account-email/[name].tsx
+++ src/app/(auth)/create-account/account-email/[name].tsx
@@ -1,21 +1,14 @@
 import { Feather } from '@expo/vector-icons';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Link, useLocalSearchParams } from 'expo-router';
-import React from 'react';
 import { useForm } from 'react-hook-form';
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
+import { Text, View } from 'react-native';
 import { z } from 'zod';
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { ProgressBar } from '@/components/ProgressBar';
+import { KeyBoardAvoidingViewLayout } from '@/layout/KeyboardAvoidingViewLayout';
 
 const emailAccountSchema = z.object({
   email: z
@@ -44,49 +37,38 @@ export default function EmailAccount() {
   const watchEmail = watch('email');
 
   return (
-    <KeyboardAvoidingView
-      className="flex-1 bg-brand"
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 60 : 0}>
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View className="flex-1">
-          <View className="mt-4 space-y-4 px-6">
-            <ProgressBar progress={80} />
-            <Text className="font-MontserratSemiBold text-base text-white">
-              2º Passo - Seus dados de acesso
-            </Text>
-          </View>
+    <KeyBoardAvoidingViewLayout>
+      <View className="mt-4 space-y-4 px-6">
+        <ProgressBar progress={80} />
+        <Text className="font-MontserratSemiBold text-base text-white">
+          2º Passo - Seus dados de acesso
+        </Text>
+      </View>
 
-          <View className="mt-8 flex-1 items-center rounded-t-2xl bg-white px-2">
-            <View className="mt-6 w-full flex-1 items-center space-y-4">
-              <Text className="font-MontserratBold text-lg">Qual é o seu e-mail?</Text>
-              <Input
-                control={control}
-                name="email"
-                placeholder="Digite seu e-mail"
-                isValid={isValid}
-                textAlign="center"
-                variant="unstyled"
-                autoCapitalize="none"
-                error={errors.email?.message}
-              />
-            </View>
-
-            <View className="absolute bottom-20 right-6">
-              <Link asChild href={`/(auth)/create-account/account-password/${name}/${watchEmail}`}>
-                <Button disabled={!isValid} variant="rounded">
-                  <Feather
-                    name="arrow-right"
-                    size={24}
-                    color="#fff"
-                    backgroundColor="transparent"
-                  />
-                </Button>
-              </Link>
-            </View>
-          </View>
+      <View className="mt-8 h-full flex-1 items-center rounded-t-2xl bg-white px-2">
+        <View className="mt-6 w-full flex-1 items-center space-y-4">
+          <Text className="font-MontserratBold text-lg">Qual é o seu e-mail?</Text>
+          <Input
+            control={control}
+            name="email"
+            placeholder="Digite seu e-mail"
+            isValid={isValid}
+            textAlign="center"
+            keyboardType="email-address"
+            variant="unstyled"
+            autoCapitalize="none"
+            error={errors.email?.message}
+          />
         </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+
+        <View className="mb-8 mr-5 w-full items-end">
+          <Link asChild href={`/(auth)/create-account/account-password/${name}/${watchEmail}`}>
+            <Button disabled={!isValid} variant="rounded">
+              <Feather name="arrow-right" size={24} color="#fff" backgroundColor="transparent" />
+            </Button>
+          </Link>
+        </View>
+      </View>
+    </KeyBoardAvoidingViewLayout>
   );
 }
--- src/app/(auth)/create-account/account-password/[...user].tsx
+++ src/app/(auth)/create-account/account-password/[...user].tsx
@@ -3,20 +3,13 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { router, useLocalSearchParams } from 'expo-router';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
-import {
-  Keyboard,
-  KeyboardAvoidingView,
-  Platform,
-  Text,
-  TouchableWithoutFeedback,
-  View,
-} from 'react-native';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Text, View } from 'react-native';
 import { z } from 'zod';
 
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
 import { ProgressBar } from '@/components/ProgressBar';
+import { KeyBoardAvoidingViewLayout } from '@/layout/KeyboardAvoidingViewLayout';
 import { UserQuery } from '@/queries/user';
 
 const passwordAccountSchema = z.object({
@@ -35,14 +28,9 @@ type User = [name: string, email: string];
 
 export default function PasswordAccount() {
   const [showPassword, setShowPassword] = useState(false);
-  const insets = useSafeAreaInsets();
   const { user } = useLocalSearchParams<{ user: User }>();
   const [name, email] = user;
-  const { mutate: createUser, isLoading } = UserQuery.Create({
-    onSuccess() {
-      router.replace('/(auth)/create-account/feedback');
-    },
-  });
+  const { mutate: createUser, isLoading, isSuccess } = UserQuery.Create();
 
   const {
     control,
@@ -59,66 +47,62 @@ export default function PasswordAccount() {
       s_password: password,
       s_name: name,
     });
+
+    if (isSuccess) {
+      router.replace('/(auth)/create-account/feedback');
+    }
   };
 
   return (
-    <KeyboardAvoidingView
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={Platform.OS === 'ios' ? 50 : 0}
-      className="flex-1 bg-brand">
-      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View className="flex-1">
-          <View className="mt-4 space-y-4 px-6">
-            <ProgressBar progress={100} />
-            <Text className="font-MontserratSemiBold text-base text-white">
-              3º Passo - Seus dados de acesso
-            </Text>
-          </View>
+    <KeyBoardAvoidingViewLayout>
+      <View className="mt-4 space-y-4 px-6">
+        <ProgressBar progress={100} />
+        <Text className="font-MontserratSemiBold text-base text-white">
+          3º Passo - Seus dados de acesso
+        </Text>
+      </View>
 
-          <View className="mt-8 flex-1 items-center rounded-t-2xl bg-white px-2">
-            <View className="mt-6 w-full flex-1 items-center space-y-4">
-              <Text className="font-MontserratBold text-lg">Vamos criar uma senha de acesso:</Text>
-              <Input
-                control={control}
-                name="password"
-                placeholder="Digite sua senha"
-                isValid={isValid}
-                variant="unstyled"
-                textAlign="center"
-                secureTextEntry={!showPassword}
-                autoCapitalize="none"
-                error={errors.password?.message}
-              />
-            </View>
-
-            <View
-              style={{
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-                marginBottom: insets.bottom + 24,
-              }}
-              className="w-full items-center px-4">
-              <View>
-                <Button
-                  className="w-full"
-                  onPress={() => setShowPassword((prevState) => !prevState)}
-                  variant="ghost">
-                  <Text className="font-MontserratBold text-sm text-brand">
-                    {showPassword ? 'ocultar senha' : 'mostrar senha'}
-                  </Text>
-                </Button>
-              </View>
-              <Button
-                isLoading={isLoading}
-                onPress={handleSubmit(onSubmit)}
-                disabled={!isValid}
-                variant="rounded">
-                <Feather name="arrow-right" size={24} color="#fff" backgroundColor="transparent" />
-              </Button>
-            </View>
-          </View>
+      <View className="mt-8 flex-1 items-center rounded-t-2xl bg-white px-2">
+        <View className="mt-6 w-full flex-1 items-center space-y-4">
+          <Text className="font-MontserratBold text-lg">Vamos criar uma senha de acesso:</Text>
+          <Input
+            control={control}
+            name="password"
+            placeholder="Digite sua senha"
+            isValid={isValid}
+            variant="unstyled"
+            textAlign="center"
+            secureTextEntry={!showPassword}
+            autoCapitalize="none"
+            error={errors.password?.message}
+          />
         </View>
-      </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
+
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}
+          className="mb-8 w-full items-center px-4">
+          <View>
+            <Button
+              className="w-full"
+              onPress={() => setShowPassword((prevState) => !prevState)}
+              variant="ghost">
+              <Text className="font-MontserratBold text-sm text-brand">
+                {showPassword ? 'ocultar senha' : 'mostrar senha'}
+              </Text>
+            </Button>
+          </View>
+          <Button
+            isLoading={isLoading}
+            onPress={handleSubmit(onSubmit)}
+            disabled={!isValid}
+            variant="rounded">
+            <Feather name="arrow-right" size={24} color="#fff" backgroundColor="transparent" />
+          </Button>
+        </View>
+      </View>
+    </KeyBoardAvoidingViewLayout>
   );
 }
