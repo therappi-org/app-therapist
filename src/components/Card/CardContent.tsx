9c08da44ec6e015c93487a9d53b5ee1416eeb193
Ian Felix <ianfelix030@gmail.com>
2024-01-28 01:22:15 -0300

    fix: adjust card component

--- src/components/Card/CardContent.tsx
+++ src/components/Card/CardContent.tsx
@@ -1,22 +1,21 @@
+import clsx from 'clsx';
 import { Image, ImageProps } from 'expo-image';
 import React from 'react';
-import { StyleProp, Text, View, ViewStyle } from 'react-native';
-
-type CardContentProps = {
+import { Text, View, ViewProps } from 'react-native';
+interface CardContentProps extends ViewProps {
   title?: string;
   subtitle?: string;
   image?: ImageProps;
-  style?: StyleProp<ViewStyle>;
-};
+}
 
-export const CardContent = ({ title, subtitle, image, style }: CardContentProps) => {
+export const CardContent = ({ title, subtitle, image, className, ...props }: CardContentProps) => {
   return (
-    <View className="flex-row items-center gap-2" style={style}>
+    <View className={clsx('flex-row items-center gap-1', className)} {...props}>
       {image && <Image className="h-14 w-14" {...image} />}
       <View className="flex flex-col gap-1">
         {title && <Text className="font-MontserratBold text-sm text-brand-100">{title}</Text>}
         {subtitle && (
-          <Text className="font-MontserratSemiBold text-sm text-gray-600 ">{subtitle}</Text>
+          <Text className="font-MontserratSemiBold text-sm text-gray-600">{subtitle}</Text>
         )}
       </View>
     </View>
--- src/components/Card/CardRoot.tsx
+++ src/components/Card/CardRoot.tsx
@@ -1,12 +1,18 @@
-import { View } from 'react-native';
+import clsx from 'clsx';
+import { View, ViewProps } from 'react-native';
 
-type CardRootProps = {
+interface CardRootProps extends ViewProps {
   children: React.ReactNode;
-};
+}
 
-export const CardRoot = ({ children }: CardRootProps) => {
+export const CardRoot = ({ children, className, ...props }: CardRootProps) => {
   return (
-    <View className="w-full max-w-full flex-row items-center justify-between rounded-2xl bg-white p-3 shadow-sm">
+    <View
+      className={clsx(
+        'w-full max-w-full flex-row items-center justify-between rounded-2xl bg-white p-3 shadow-sm',
+        className
+      )}
+      {...props}>
       {children}
     </View>
   );
