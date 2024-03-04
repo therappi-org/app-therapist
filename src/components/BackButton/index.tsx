bd7e47efa1fa7ac859b3ee8d4ae4605f1958a2d9
Ian Felix <ianfelix030@gmail.com>
2024-03-04 00:53:34 -0300

    refactor:  BackButton component to accept an optional iconName prop

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -3,10 +3,14 @@ import { router } from 'expo-router';
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
 
-export const BackButton = () => {
+type BackButtonProps = {
+  iconName?: string;
+};
+
+export const BackButton = ({ iconName = 'arrow-left' }: BackButtonProps) => {
   return (
     <TouchableOpacity className="android:mr-4" onPress={() => router.back()}>
-      <Feather name="arrow-left" size={24} color="#fff" backgroundColor="transparent" />
+      <Feather name={iconName as any} size={24} color="#fff" backgroundColor="transparent" />
     </TouchableOpacity>
   );
 };
