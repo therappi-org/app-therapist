c4fcf1c7b1873178fde18e7d080876781c70472f
Ian Felix <ianfelix030@gmail.com>
2024-03-22 11:36:10 -0300

    feat: Add border styling to CardCheckbox component

--- src/components/Card/CardCheckbox.tsx
+++ src/components/Card/CardCheckbox.tsx
@@ -14,6 +14,7 @@ export const CardCheckbox = ({ isChecked, setChecked, className, ...props }: Car
     <Checkbox
       value={isChecked}
       className={cn('h-4 w-4 rounded-full', className)}
+      style={{ borderColor: colors.brand.DEFAULT, borderRadius: 10 }}
       color={colors.brand.DEFAULT}
       onValueChange={setChecked}
       {...props}
