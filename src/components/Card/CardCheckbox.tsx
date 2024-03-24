26eedb1d9d8f7f394ef290992c6e86ea958da143
Ian Felix <ianfelix030@gmail.com>
2024-03-24 13:54:03 -0300

    feat: add custom checkbox component

--- src/components/Card/CardCheckbox.tsx
+++ src/components/Card/CardCheckbox.tsx
@@ -1,23 +1,25 @@
-import Checkbox from 'expo-checkbox';
+import { FontAwesome } from '@expo/vector-icons';
 import React from 'react';
+import { Pressable } from 'react-native';
 
 import colors from '@/theme/colors';
 import { cn } from '@/utils/lib';
 
-type CardCheckboxProps = React.ComponentProps<typeof Checkbox> & {
-  isChecked?: boolean;
-  setChecked?: React.Dispatch<React.SetStateAction<boolean>>;
+type CardCheckboxProps = React.ComponentProps<typeof Pressable> & {
+  isChecked: boolean;
+  setChecked: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
 export const CardCheckbox = ({ isChecked, setChecked, className, ...props }: CardCheckboxProps) => {
   return (
-    <Checkbox
-      value={isChecked}
-      className={cn('h-4 w-4 rounded-full', className)}
-      style={{ borderColor: colors.brand.DEFAULT, borderRadius: 10 }}
-      color={colors.brand.DEFAULT}
-      onValueChange={setChecked}
-      {...props}
-    />
+    <Pressable
+      className={cn(
+        'flex h-6 w-6 items-center justify-center rounded-full border-2 border-brand bg-transparent',
+        className
+      )}
+      onPress={() => setChecked(!isChecked)}
+      {...props}>
+      {isChecked && <FontAwesome name="circle" size={12} color={colors.brand.DEFAULT} />}
+    </Pressable>
   );
 };
