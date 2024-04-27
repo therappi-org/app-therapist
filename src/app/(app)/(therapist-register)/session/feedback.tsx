b53fcfedff819bb2d8061108473dea5b697d3b2b
Ian Felix <ianfelix030@gmail.com>
2024-04-27 18:10:43 -0300

    refactor: adjust text size for small devices

--- src/app/(app)/(therapist-register)/session/feedback.tsx
+++ src/app/(app)/(therapist-register)/session/feedback.tsx
@@ -1,25 +1,30 @@
 import { Image } from 'expo-image';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Dimensions, Text, View } from 'react-native';
 
 import { Button } from '@/components/Button';
 import { CardStack } from '@/layout/CardStack';
 import { storeData } from '@/utils/asyncStoreData';
 import { THERAPIST_REGISTERED_KEY } from '@/utils/constants';
+import { cn } from '@/utils/lib';
 
 export default function TherapyFeedback() {
+  const { height } = Dimensions.get('window');
+  const isSmallDevice = height <= 700;
+
   return (
     <CardStack>
       <View className="flex-1 px-4 py-6">
         <View className="mt-12 flex-1 items-center justify-center">
           <Image
-            className="h-48 w-52"
+            className={cn('h-52 w-52', isSmallDevice && 'h-40 w-40')}
             alt="Logo"
             source={require('@/assets/images/successful.svg')}
           />
 
           <View className="mt-10 gap-2">
-            <Text className="text-center font-MontserratBold text-xl text-gray-800">
+            <Text
+              className={cn('text-center font-MontserratBold text-xl', isSmallDevice && 'text-sm')}>
               Maravilha! A Terapia foi cadastrada com sucesso!
             </Text>
           </View>
